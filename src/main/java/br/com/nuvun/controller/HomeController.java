574b7bdf2f93f69bdbb3eb3b9e112fb850981856
Raul Candido <raul.cand@hotmail.com>
2021-04-04 22:17:51 -0300

    Continuando no primeiro crud com Thymeleaf
    Concluindo visualização de itens de produtos.

--- src/main/java/br/com/nuvun/controller/HomeController.java
+++ src/main/java/br/com/nuvun/controller/HomeController.java
@@ -2,37 +2,44 @@ package br.com.nuvun.controller;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.nuvun.model.Pedido;
+import br.com.nuvun.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 
 	@GetMapping("/home")
 	public String home(Model model) {
-		
-		List<Pedido> pedidos = criarPedidos();
+		List<Pedido> pedidos = buscarPedidos();
+		//criarPedido();
 		model.addAttribute("pedidos", pedidos);		
 		return "home";
 	}
 
-	private List<Pedido> criarPedidos() {
-		Pedido pedido = new Pedido();
-
-		pedido.setNomeProduto("Novo Echo Dot (4ª Geração): Smart Speaker com Alexa");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/714Rq4k05UL._AC_SL1000_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Novo-Echo-Dot-4%C2%AA-gera%C3%A7%C3%A3o/dp/B084DWCZY6/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=echo&qid=1616851080&sr=8-1");
-		pedido.setDescricao("Conheça o novo Echo Dot: nosso smart speaker com Alexa de maior sucesso ainda melhor. O novo design de áudio com direcionamento frontal (1 speaker de 1,6\") garante mais graves e um som completo");
-		pedido.setDataEntrega(LocalDate.now());
-		pedido.setValor(new BigDecimal(154));
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+	private List<Pedido> buscarPedidos() {		
+		List<Pedido> pedidos = pedidoRepository.findAll();
 		return pedidos;
+	}
+	
+	private void criarPedido() {	
+		Pedido pedido = new Pedido();
+		pedido.setDataEntrega(LocalDate.now());
+		pedido.setDescricao("Caneca estilizada do mundo de Alice");
+		pedido.setNomeProduto("Caneca Alice no País das Maravilhas Gato");
+		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/31%2BwpubOrlL._AC_.jpg");
+		pedido.setUrlProduto("https://www.amazon.com.br/Caneca-Alice-Pa%C3%ADs-Maravilhas-Gato/dp/B085BRM5C5/?_encoding=UTF8&pd_rd_w=gsWuu&pf_rd_p=1c6d1c78-c691-4292-af98-b6a56e268b64&pf_rd_r=RQS6J517X3VN62JXDB0K&pd_rd_r=44edb74b-1983-4912-a680-6a4a29363504&pd_rd_wg=niuUY&ref_=pd_gw_crs_wish");
+		pedido.setValor(new BigDecimal(29.90));
+		pedidoRepository.save(pedido);
 	}
 }
--- src/main/java/br/com/nuvun/model/Pedido.java
+++ src/main/java/br/com/nuvun/model/Pedido.java
@@ -3,6 +3,12 @@ package br.com.nuvun.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,8 +16,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 public class Pedido {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nomeProduto;
 	private BigDecimal valor;
 	private LocalDate dataEntrega;
