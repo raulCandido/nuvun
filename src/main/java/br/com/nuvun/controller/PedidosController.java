23c2cb039867a81b98e24462226a66e4dc42bab2
raulCandido <raul.cand@hotmail.com>
2021-04-08 22:03:30 -0300

    Inclusao de validacoes, inicio da tela de login

--- src/main/java/br/com/nuvun/controller/PedidosController.java
+++ src/main/java/br/com/nuvun/controller/PedidosController.java
@@ -1,7 +1,10 @@
 package br.com.nuvun.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,21 +16,27 @@ import br.com.nuvun.repository.PedidoRepository;
 @Controller
 @RequestMapping("pedido")
 public class PedidosController {
-	
+
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
-	
+
 	@GetMapping("formulario")
-	public String abrirFormulario() {
+	public String abrirFormulario(@Valid PedidoDto pedidoDto, BindingResult result) {
 		return "pedido/formulario";
 	}
-	
+
 	@PostMapping("novo")
-	public String cadastrarPedido(PedidoDto pedidoDto) {
+	public String cadastrarPedido(@Valid PedidoDto pedidoDto, BindingResult result) {
+		if (result.hasErrors()) {
+			return "pedido/formulario";
+		}
 		Pedido pedido = pedidoDto.converterPedidoDtoEmPedido();
 		pedidoRepository.save(pedido);
 		return "pedido/formulario";
 	}
 	
+	@GetMapping("logoff")
+	public String sairDoSistema() {
+		return "pedido/login";
+	}
 }
--- src/main/java/br/com/nuvun/model/dto/PedidoDto.java
+++ src/main/java/br/com/nuvun/model/dto/PedidoDto.java
@@ -1,13 +1,21 @@
 package br.com.nuvun.model.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import br.com.nuvun.model.Pedido;
 import lombok.Data;
 
 @Data
 public class PedidoDto {
+	@NotBlank(message = "Campo Obrigatório")
 	private String nomeProduto;
+
+	@NotBlank(message = "Campo Obrigatório")
 	private String urlProduto;
+
+	@NotBlank(message = "Campo Obrigatório")
 	private String urlImagemProduto;
+
 	private String descricaoProduto;
 
 	public Pedido converterPedidoDtoEmPedido() {
